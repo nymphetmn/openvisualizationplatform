0f07616570d9c764429c75e358933990b11d2148
cdhowie <cdhowie@fa2d8270-aa4b-0410-be4a-fb59b02ed7c8>
2008-07-16 15:55:33 +0000

    * OpenVP.Manager/OpenVP.Manager.Metadata/StaticChannel.cs: Implement   hackish deserialization. * OpenVP.Manager/OpenVP.Manager.Metadata/IEventGenerator.cs: Made   non-generic since generic usage would require reflection.

--- OpenVP.Manager/OpenVP.Manager/OpenVP.Manager.Metadata/StaticChannel.cs
+++ OpenVP.Manager/OpenVP.Manager/OpenVP.Manager.Metadata/StaticChannel.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace OpenVP.Manager.Metadata {
     public class StaticChannel<T> : IChannel<T> where T : Event {
@@ -27,6 +28,56 @@ namespace OpenVP.Manager.Metadata {
             this.events = new List<T>(source);
             
             this.CreateTimeline();
+        }
+        
+        public StaticChannel(XElement channel) : this(DeserializeEvents(channel)) {
+        }
+        
+        private static IEnumerable<T> DeserializeEvents(XElement c) {
+            foreach (XElement i in c.Elements("event")) {
+                foreach (T e in DeserializeEvent(i)) {
+                    yield return e;
+                }
+            }
+        }
+        
+        private static IEnumerable<T> DeserializeEvent(XElement e) {
+            string type = (string) e.Attribute("type");
+            if (type == null)
+                throw new ArgumentException("Event has no type.");
+            
+            Type t = Type.GetType(type);
+            if (t == null)
+                throw new ArgumentException("Unknown type: " + type);
+            if (!typeof(T).IsAssignableFrom(t))
+                throw new ArgumentException("Event type " + type + 
+                                            " cannot be converted to channel type " + typeof(T).FullName);
+            
+            Event evnt = (Event) Activator.CreateInstance(t, e);
+            
+            return ProcessEvent(evnt);
+        }
+        
+        private static IEnumerable<T> ProcessEvent(Event evnt) {
+            IEventGenerator gen = evnt as IEventGenerator;
+            if (gen != null) {
+                if (gen.GeneratorMode != GeneratorMode.Discard) {
+                    if (typeof(T).IsAssignableFrom(gen.GetType()))
+                        yield return (T) gen;
+                    else if (gen.GeneratorMode == GeneratorMode.Retain)
+                        throw new ArgumentException("Generator cannot be retained.");
+                }
+                
+                foreach (Event e in gen) {
+                    foreach (T i in ProcessEvent(e)) {
+                        yield return i;
+                    }
+                }
+            } else if (typeof(T).IsAssignableFrom(gen.GetType())) {
+                yield return (T) evnt;
+            } else {
+                throw new ArgumentException("Event type is not compatible with this channel.");
+            }
         }
         
         private void CreateTimeline() {
